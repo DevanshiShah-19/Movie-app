e4e16dc4f4906bb446a5c31aee6672f5860d8676
DevanshiShah-19 <devanshishah.tcs2024@gmail.com>
2024-03-28 11:31:46 +0530

    Final Moovie Project

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -21,12 +21,8 @@ const Footer = () => {
           <li className="menuItem">FAQ</li>
         </ul>
         <div className="infoText">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur.
+          From binge-worthy series to blockbuster hits, our website offers a one-stop destination for all your TV and movie cravings. Explore, discover, and immerse yourself in the ultimate entertainment
+          experience.
         </div>
         <div className="socialIcons">
           <span className="icon">
